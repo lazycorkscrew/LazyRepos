1452cf32937c85ebcc610761e1ede8b68538abfe
lazycorkscrew <54800143+lazycorkscrew@users.noreply.github.com>
2019-09-02 21:26:16 +0400

    Added

---
+++ EPAM_Tasks/EPAM_Tasks/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+
+namespace EPAM_Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] massiv = { 3, 4, 12, 7, 8, 5, 11, 9, 3, 10, 2, 1, 15, 8, 13, 0, 14, 6 };
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Задание 1 - Сортировка целочисленного массива.");
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+            //Показывает исходный массив.
+            Console.WriteLine("Оригинальный массив:");
+            ShowArray(massiv);
+
+            //Демонстрация сортировки без изменения оригинала.
+            Console.WriteLine("\nОтсортированная копия:");
+            ShowArray(Tasks.Sort(massiv, true));
+
+            Console.WriteLine("\nОригинальный массив:");
+            ShowArray(massiv);
+
+            //Демонстрация сортировки оригинала.
+
+            Console.WriteLine("\nСортируем оригинал:");
+            Tasks.Sort(massiv);
+            //можно использовать Array.Sort<int>(massiv);
+            ShowArray(massiv);
+
+            Console.WriteLine("\nВыводим оригинальный массив:");
+            ShowArray(massiv);
+
+            Console.WriteLine("\nУдаляем третий элемент:");
+            massiv = Tasks.Remove(massiv, 2, 2);
+            ShowArray(massiv);
+
+            Console.WriteLine("\n\nСледующее задание >>");
+            Console.ReadKey();
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("\n\nЗадание 2 - Поиск в отсортированном массиве.");
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Введите искомое число: \n > ");
+                    if (Tasks.Find(massiv, Convert.ToInt32(Console.ReadLine())) >= 0)
+                        Console.WriteLine("Нашли");
+                    else Console.WriteLine("не нашли");
+                    break;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Введите корректное значение. Входными данными должно являться целое число.");
+                }
+            }
+
+            Console.WriteLine("\nСледующее задание >>");
+            Console.ReadKey();
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("\n\nЗадание 3 - Поиск в строке слов, встречающихся только один раз.");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            string str = "один два три два пять пять четыре двоа";
+            Console.WriteLine(str);
+            string[] words = Tasks.DistinctStrings(str);
+            foreach (string word in words)
+            {
+                Console.Write($"{word} ");
+            }
+
+            Console.WriteLine("\nСледующее задание >>");
+            Console.ReadKey();
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("\n\nЗадание 4 - Вычисление факториала.");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("Введите число: \n > ");
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine($"Факториал числа равен {Tasks.Factorial(Convert.ToUInt16(Console.ReadLine()))}");
+                    break;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Введите корректное значение. Входными данными должно являться целое число.");
+                }
+            }
+
+            Console.WriteLine("\nСледующее задание >>");
+            Console.ReadKey();
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("\n\nЗадание 5 - Скобочная последовательность.");
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+            string[] forValidate = {
+                "namespace Test{ class Program{ static void Main(string[] args) {Console.WriteLine(\"Hello, world!\");}}}",
+                "{([](){})[{}()]{[]()}}",
+                "{([](){})[{)()]{[]()}}",
+                "{([](){}))([{}()]}[]()}}",
+            };
+
+            foreach (string s in forValidate)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(s);
+                Console.ForegroundColor = ConsoleColor.Gray;
+                if (Tasks.Validate(s.ToCharArray()))
+                    Console.WriteLine("\n - Правильно.");
+                else Console.WriteLine("\n - Неправильно.");
+            }
+            Console.ReadKey();
+        }
+
+        static void ShowArray(int[] array)
+        {
+            foreach (int v in array)
+            {
+                Console.Write($"{v} ");
+            }
+        }
+    }
+}
+
---
+++ EPAM_Tasks/Tasks/Tasks.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EPAM_Tasks
+{
+    public static class Tasks
+    {
+        //Задание 1
+        //Пузырьковая сортировка целочисленного массива с возвращением значения.
+        //Вместо этой функции можно использовать Array.Sort<int>(int [] array);
+        public static int[] Sort(int[] arrayToSort, bool onlyShow = false)
+        {
+            int[] arrayToWork = (onlyShow ? (int[])arrayToSort.Clone() : arrayToSort);
+            int temp;
+
+            for (int i = 1; i != arrayToWork.Length; i++)
+            {
+                for (int j = 0; j < arrayToWork.Length - i; j++)
+                {
+                    if (arrayToWork[j] > arrayToWork[j + 1])
+                    {
+                        temp = arrayToWork[j + 1];
+                        arrayToWork[j + 1] = arrayToWork[j];
+                        arrayToWork[j] = temp;
+                    }
+                }
+            }
+            return arrayToWork;
+        }
+
+        //Задание 2
+        //Бинарный поиск в массиве. При успешном поиске возвращает индекс первого вхождения
+        //Если ничего не нашёл = возвращает -1
+        //Вместо этой функции можно использовать Array.BinarySearch<int>(int [] array, int value);
+        public static int Find(int[] array, int value)
+        {
+            int modific = array.Length / 2; //Модификатор позиции.
+            int findPosition = modific; //текущая позиция равная половине массива.
+            sbyte lastIteration = 0;
+
+            while (lastIteration < 2)
+            {
+                if (modific != 1)
+                {
+                    if (modific / 2 != 0)
+                        modific /= 2;
+                    else modific = 1;
+                }
+                else lastIteration++;
+
+                if (array[findPosition - 1] < value)
+                {
+                    findPosition += modific;
+                }
+                else if (array[findPosition - 1] > value)
+                {
+                    findPosition -= modific;
+                }
+                else
+                {
+                    for (int i = findPosition - 1; i > 0; i--)
+                    {
+                        if (array[i] != value)
+                        {
+                            Console.WriteLine($"{array[i + 1]} [{i + 1}]");
+                            return i + 1;
+                        }
+                    }
+                    Console.WriteLine($"{array[findPosition - 1]} [{findPosition - 1}]");
+                    return findPosition - 1;
+                }
+            }
+            return -1;
+        }
+
+        //Задание 3
+        public static string[] DistinctStrings(string bigString)
+        {
+            string[] words = bigString.Split(new char[] { ' ', '.', ',', '?', '!', ':', ';', '-' }, StringSplitOptions.RemoveEmptyEntries);
+            List<string> distinctWords = new List<string>();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                int found = -1;
+                for (int j = 0; j < distinctWords.Count; j++)
+                {
+                    if (words[i] == distinctWords[j])
+                    {
+                        found = j;
+                        break;
+                    }
+                }
+                if (found >= 0) distinctWords.RemoveAt(found);
+                else distinctWords.Add(words[i]);
+            }
+            return distinctWords.ToArray();
+        }
+
+        //Задание 4
+        public static ulong Factorial(ushort value)
+        {
+            {
+                ulong c = 1;
+                for (uint i = 1; i <= value; i++)
+                {
+                    c *= i;
+                }
+                return c;
+            }
+        }
+
+
+
+        //Задание 5
+        public static bool Validate(char[] charsTemp)
+        {
+            //Создаём массив, содержащий в себе только скобки, без сторонних символов.
+
+
+            int capacity = 0;
+            for (int i = 0; i < charsTemp.Length; i++)
+            {
+                if (charsTemp[i] == '(' || charsTemp[i] == ')' || charsTemp[i] == '[' || charsTemp[i] == ']' || charsTemp[i] == '{' || charsTemp[i] == '}')
+                    capacity++;
+            }
+            char[] charsArr = new char[capacity];
+            int index = 0;
+            for (int i = 0; i < charsTemp.Length; i++)
+            {
+                if (charsTemp[i] == '(' || charsTemp[i] == ')' || charsTemp[i] == '[' || charsTemp[i] == ']' || charsTemp[i] == '{' || charsTemp[i] == '}')
+                {
+                    charsArr[index] = charsTemp[i];
+                    index++;
+                }
+            }
+
+            //Если число символов нечётное - о валидности не может идти и речи.
+            if (charsArr.Length % 2 != 0)
+                return false;
+
+            //List<char> chars = charsArr.ToList<char>();
+
+            //По умолчанию, пустая строка является валидной.
+            if (charsArr.Length == 0)
+                return true;
+
+            char[] chars = charsArr;
+
+            //Если последовательность начинается с закрытой скобки, или заканчивается открытой - 
+            //о валидности не может идти и речи.
+            if (chars[0] == ']' || chars[0] == ')' || chars[0] == '}')
+                return false;
+            if (chars[chars.Length - 1] == '[' || chars[chars.Length - 1] == '(' || chars[chars.Length - 1] == '{')
+                return false;
+
+            //Убираем в цикле все открывающиеся, и сразу закрывающиеся скобки (до тех пор, пока они убираются).
+            bool somethingsDeleted = true;
+            while (somethingsDeleted)
+            {
+                somethingsDeleted = false;
+                for (uint i = 0; i < chars.Length - 1; i++)
+                {
+                    //Если в списке не осталось больше элементов - то строка валидная.
+                    if (chars.Length == 0)
+                        return true;
+
+                    if ((chars[i] == '[' && chars[i + 1] == ']') ||
+                        (chars[i] == '(' && chars[i + 1] == ')') ||
+                        (chars[i] == '{' && chars[i + 1] == '}'))
+                    {
+                        chars = Remove(chars, i, 2);
+                        GC.Collect();
+                        somethingsDeleted = true;
+                        i--;
+                    }
+                }
+            }
+            return (chars.Length == 0 ? true : false);
+        }
+
+
+        //Функция удаления элемента массива
+        public static T[] Remove<T>(T[] array, uint index, int count)
+        {
+            T[] newArray = new T[array.Length - count];
+            for (uint i = 0; i < array.Length - count; i++)
+            {
+                if (i >= index)
+                    newArray[i] = array[i + count];
+                else newArray[i] = array[i];
+            }
+            return newArray;
+        }
+    }
+}
